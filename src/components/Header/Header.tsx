c4639802b662613bbdde631645512a382e163fde
Felipe <diegoingasilva@gmail.com>
2024-10-02 17:53:00 -0500

    0.0.0.8

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -25,11 +25,11 @@ export function Header() {
     <div className="flex-1 flex justify-center">
       <div className="hidden md:flex md:space-x-6">
         {[
-          { id: 1, name: "¿Qué es Repify?", idLink: "/que-es-repify" },
-          { id: 2, name: "Beneficios", idLink: "/beneficios" },
-          { id: 3, name: "Nuestro Equipo", idLink: "/nuestro-equipo" },
-          { id: 4, name: "Testimonios", idLink: "/testimonios" },
-          { id: 5, name: "Colaboración", idLink: "/colaboracion" }
+          { id: 1, name: "¿Qué es Repify?", idLink: "#features" },
+          { id: 2, name: "Beneficios", idLink: "#beneficios" },
+          { id: 3, name: "Nuestro Equipo", idLink: "#nuestroequipo" },
+          { id: 4, name: "Testimonios", idLink: "#testimonios" },
+          { id: 5, name: "Colaboración", idLink: "#servicios" }
         ].map(({ id, name, idLink }) => (
           <div key={id} className="px-2 transition-all duration-500 ease-in-out"> {/* Espaciado más ajustado */}
             <Link href={idLink} className="text-lg hover:text-secondary">{name}</Link>
